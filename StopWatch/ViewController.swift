2476696b4e1b3d8de6e5022aecf9e34363c3bfc7
Apple <fibo2944@yahoo.co.jp>
2019-01-28 17:56:40 +0900

    基本的な機能を作成 スタートの条件分岐とストップボタンは完成 レイアウト、ポーズ機能が未完成

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var minLabel: UILabel!
+    @IBOutlet weak var secLabel: UILabel!
+    @IBOutlet weak var miSecLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    weak var timer: Timer!
+    var startTime = Date()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +29,71 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func startButtonTapped(_ sender: Any) {
+        startTime = Date()
+        
+        if startButton.currentTitle == "Start!"{
+            timer = Timer.scheduledTimer(
+                timeInterval: 0.01,
+                target: self,
+                selector: #selector(self.timeCounter),
+                userInfo: nil,
+                repeats: true)
 
+            
+            startButton.setTitle("Pause", for: UIControlState.normal)
+            
+        }else if startButton.currentTitle == "Pause"{
+            timer.invalidate()
+            
+            startButton.setTitle("Restart", for: UIControlState.normal)
+            
+        }else{
+            timer = Timer.scheduledTimer(
+                timeInterval: 0.01,
+                target: self,
+                selector: #selector(self.timeCounter),
+                userInfo: nil,
+                repeats: true)
+
+            startButton.setTitle("Pause", for: UIControlState.normal)
+        }
+    }
+    
+    
+    @IBAction func stopButtonTapped(_ sender: Any) {
+        timer.invalidate()
+        
+        minLabel.text = "00"
+        secLabel.text = "00"
+        miSecLabel.text = "00"
+        
+        startButton.setTitle("Start!", for: UIControlState.normal)
+    }
+    
+    @objc func timeCounter(){
+        let currentTime = Date().timeIntervalSince(startTime)
+        
+        let currentMin = Int(currentTime/60)
+        let currentSec = Int(currentTime) % 60
+        let currentMiSec = Int(currentTime*100) % 100
+        
+        minLabel.text = String(format: "%02d", currentMin)
+        secLabel.text = String(format: "%02d", currentSec)
+        miSecLabel.text = String(format: "%02d", currentMiSec)
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
