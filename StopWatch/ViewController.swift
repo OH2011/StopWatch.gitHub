42f06bb09c9021edc3b5bbdbff67859048a8fa12
Apple <fibo2944@yahoo.co.jp>
2019-01-28 21:22:06 +0900

    リスタート機能完成！
    あとはレイアウトを綺麗にしたい。

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -18,10 +18,15 @@ class ViewController: UIViewController {
     
     weak var timer: Timer!
     var startTime = Date()
+    var oldMin = 0
+    var oldSec = 0
+    var oldMiSec = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +35,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func startButtonTapped(_ sender: Any) {
+        
         startTime = Date()
         
         if startButton.currentTitle == "Start!"{
@@ -49,10 +55,18 @@ class ViewController: UIViewController {
             startButton.setTitle("Restart", for: UIControlState.normal)
             
         }else{
+            oldMin = Int(minLabel.text!)!
+            oldSec = Int(secLabel.text!)!
+            oldMiSec = Int(miSecLabel.text!)!
+            
+             let oldTime = Double(oldMin*60) + Double(oldSec) + Double(oldMiSec)*0.01
+            
+            startTime = NSDate(timeInterval: -oldTime, since: startTime) as Date;
+            
             timer = Timer.scheduledTimer(
                 timeInterval: 0.01,
                 target: self,
-                selector: #selector(self.timeCounter),
+                selector: #selector(self.restartTimeCounter),
                 userInfo: nil,
                 repeats: true)
 
@@ -72,6 +86,20 @@ class ViewController: UIViewController {
     }
     
     @objc func timeCounter(){
+        let currentTime = Date().timeIntervalSince(startTime)
+        
+        let currentMin = Int(currentTime/60)
+        let currentSec = Int(currentTime) % 60
+        let currentMiSec = Int(currentTime*100) % 100
+        
+        minLabel.text = String(format: "%02d", currentMin)
+        secLabel.text = String(format: "%02d", currentSec)
+        miSecLabel.text = String(format: "%02d", currentMiSec)
+    }
+    
+    @objc func restartTimeCounter(){
+        //currentTimeにoldTimeを足すと毎回上乗せされるので、startTimeから元の経過時間をひくことにする。
+        //このアクション内にコードを書くと何回も連続で計算されるので一回しか計算したくない変数を枠外で定義して、ここの呼び出し元のアクション内で値を代入しておく。
         let currentTime = Date().timeIntervalSince(startTime)
         
         let currentMin = Int(currentTime/60)
